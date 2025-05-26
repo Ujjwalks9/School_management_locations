1c74825791eb1065bf061fea131553580d9d8eed
Ujjwalks9 <ujjwalks2709@gmail.com>
2025-05-27 00:15:51 +0530

    rechange the server.js

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const schoolRoutes = require('./routes/schoolRoutes');
+dotenv.config();
+
 const app = express();
-const schoolRoutes = require('./routes/schoolRoutes'); 
-
 app.use(express.json());
-app.use('/', schoolRoutes);
 
-const PORT = process.env.PORT || 10000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.use('/api', schoolRoutes);
+
+app.listen(process.env.PORT, () =>
+  console.log(`Server running on port ${process.env.PORT}`)
+);
