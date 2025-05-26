a04da60bb86208b65d66ba40a0ef39aaa145dec6
Ujjwalks9 <ujjwalks2709@gmail.com>
2025-05-26 22:37:53 +0530

    port fixed

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 const express = require('express');
-const dotenv = require('dotenv');
-const schoolRoutes = require('./routes/schoolRoutes');
-dotenv.config();
-
 const app = express();
+const schoolRoutes = require('./routes/schoolRoutes'); 
+
 app.use(express.json());
+app.use('/', schoolRoutes);
 
-app.use('/api', schoolRoutes);
-
-app.listen(process.env.PORT, () =>
-  console.log(`Server running on port ${process.env.PORT}`)
-);
+const PORT = process.env.PORT || 10000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
